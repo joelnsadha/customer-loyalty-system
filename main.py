2d243a1004c8bf8f1f12148967c11fd461ad46d7
joelnsadha <stonedev704@gmail.com>
2024-04-11 07:14:44 -0400

    Added Transaction Class

--- main.py
+++ main.py
@@ -17,10 +17,30 @@ class LoyaltyPoints:
         else:
             print("Not enough points!")
 
-    def retieve_points(self):
-        pass
+    def show_points(self):
+        return self.points
 
 
 class VipCustomer(Customer, LoyaltyPoints):
     def __init__(self, customer_id, name):
         Customer.__init__(self, customer_id, name)
+        LoyaltyPoints.__init__(self)
+
+
+class Transaction:
+    def __init__(self, transaction_id, customer, amount):
+        self.transaction_id = transaction_id
+        self.customer = customer
+        self.amount = amount
+
+
+class TransactionHistory:
+    def __init__(self):
+        self.transactions = []
+
+    def add_transaction(self, transaction):
+        self.transactions.append(transaction)
+
+    def show_transactions(self):
+        for transaction in self.transactions:
+            print(transaction.transaction_id, transaction.customer, transaction.amount)
